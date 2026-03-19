a752560e8995fe71532c5f9137440d2fa41011e2
Caleb Vatral <cvatral@tnstate.edu>
2026-03-18 19:47:49 -0500

    Add basic spawn mechanic

--- Scripts/level_manager.gd
+++ Scripts/level_manager.gd
@@ -1,18 +1,36 @@
 class_name LevelManager
 extends Node
 
+enum PlayerType {
+	Controller, Wand, Gesture
+}
+
+@export_group("Player Types")
+@export var _activePlayerType: PlayerType
+@export var _controllerPlayer: PackedScene
+
+@export_group("RNG Settings")
 @export var _seed: int = 42
 @export var _lowerBoundaryMark: Marker3D
 @export var _upperBoundaryMark: Marker3D
 
 var _rng: RandomNumberGenerator
+var _currentPlayer: Node3D
 
 func _ready() -> void:
-    _rng = RandomNumberGenerator.new()
-    _rng.seed = _seed
-    _rng.state = 0
+	_rng = RandomNumberGenerator.new()
+	_rng.seed = _seed
+	_rng.state = 0
+	_placePlayer()
+
+func _placePlayer() -> void:
+	if _activePlayerType == PlayerType.Controller:
+		_currentPlayer = _controllerPlayer.instantiate()
+		
+	get_parent().add_child.call_deferred(_currentPlayer)
+	_currentPlayer.set_deferred("global_position", GenerateShotPosition())
 
 func GenerateShotPosition() -> Vector3:
-    var x = _rng.randf_range(_lowerBoundaryMark.position.x, _upperBoundaryMark.position.x)
-    var z = _rng.randf_range(_lowerBoundaryMark.position.z, _upperBoundaryMark.position.z)
-    return Vector3(x, 0, z)
+	var x = _rng.randf_range(_lowerBoundaryMark.global_position.x, _upperBoundaryMark.global_position.x)
+	var z = _rng.randf_range(_lowerBoundaryMark.global_position.z, _upperBoundaryMark.global_position.z)
+	return Vector3(x, 0, z)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -26,81 +26,85 @@ var _currentBall: RigidBody3D
 @onready var _throwAngle: float = _defaultShotAngle
 @onready var _throwForce: float = _defaultShotForce
 var _throw_dir : Vector3
+var _moveEnabled: bool = false
 
 func _process(_delta: float) -> void:    
-    if Input.is_action_pressed("Shoot"):
-        _ballVisual.visible = true
-        _isShooting = true
-        _line3D.visible = true
-        ProcessShot()
-    if Input.is_action_just_released("Shoot"):
-        _isShooting = false
-        DoShot()
+	if Input.is_action_pressed("Shoot"):
+		_ballVisual.visible = true
+		_isShooting = true
+		_line3D.visible = true
+		ProcessShot()
+	if Input.is_action_just_released("Shoot"):
+		_isShooting = false
+		DoShot()
+	
+	if Input.is_action_just_pressed("EnableMovement"):
+		_moveEnabled = !_moveEnabled
 
 func _physics_process(delta: float) -> void:
-    if not is_on_floor():
-        velocity += get_gravity() * delta
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
-    if not _isShooting:
-        var input_dir := Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown")
-        var direction := transform.basis * Vector3(input_dir.x, 0, input_dir.y)
-        velocity.x = move_toward(velocity.x, direction.x * SPEED, ACCELERATION)
-        velocity.z = move_toward(velocity.z, direction.z * SPEED, ACCELERATION)
-    else:
-        velocity.x = 0
-        velocity.z = 0
-    var look_inp := Input.get_axis("LookRight", "LookLeft")
-    rotate_y(look_inp * LOOK_SPEED)
+	if _moveEnabled and not _isShooting:
+		var input_dir := Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown")
+		var direction := transform.basis * Vector3(input_dir.x, 0, input_dir.y)
+		velocity.x = move_toward(velocity.x, direction.x * SPEED, ACCELERATION)
+		velocity.z = move_toward(velocity.z, direction.z * SPEED, ACCELERATION)
+	else:
+		velocity.x = 0
+		velocity.z = 0
+	var look_inp := Input.get_axis("LookRight", "LookLeft")
+	rotate_y(look_inp * LOOK_SPEED)
 
-    move_and_slide()
+	move_and_slide()
 
 func SpawnBall() -> void:
-    var ball = _ballScene.instantiate() as RigidBody3D
-    get_parent().add_child(ball)
-    ball.position = _ballVisual.global_position
-    _currentBall = ball
+	var ball = _ballScene.instantiate() as RigidBody3D
+	get_parent().add_child(ball)
+	ball.position = _ballVisual.global_position
+	_currentBall = ball
 
 func ProcessShot() -> void:
-    # Adjust angle and power based on input
-    if Input.is_action_pressed("LookDown"):
-        _throwAngle = move_toward(_throwAngle, _shotAngleMin, _shotAngleAccel)
-    if Input.is_action_pressed("LookUp"):
-        _throwAngle = move_toward(_throwAngle, _shotAngleMax, _shotAngleAccel)
-    if Input.is_action_pressed("ShotPowerDown"):
-        _throwForce = move_toward(_throwForce, _shotForceMin, _shotForceAccel)
-    if Input.is_action_pressed("ShotPowerUp"):
-        _throwForce = move_toward(_throwForce, _shotForceMax, _shotForceAccel)
+	# Adjust angle and power based on input
+	if Input.is_action_pressed("LookDown"):
+		_throwAngle = move_toward(_throwAngle, _shotAngleMin, _shotAngleAccel)
+	if Input.is_action_pressed("LookUp"):
+		_throwAngle = move_toward(_throwAngle, _shotAngleMax, _shotAngleAccel)
+	if Input.is_action_pressed("ShotPowerDown"):
+		_throwForce = move_toward(_throwForce, _shotForceMin, _shotForceAccel)
+	if Input.is_action_pressed("ShotPowerUp"):
+		_throwForce = move_toward(_throwForce, _shotForceMax, _shotForceAccel)
 
-    # Calculate throw direction
-    # Character forward (basis -z) rotated up or down (basis x)
-    var forward_dir = -transform.basis.z
-    var angle_rad = deg_to_rad(_throwAngle)
-    var rotation_axis = transform.basis.x
-    _throw_dir = forward_dir.rotated(rotation_axis, angle_rad).normalized()
+	# Calculate throw direction
+	# Character forward (basis -z) rotated up or down (basis x)
+	var forward_dir = -transform.basis.z
+	var angle_rad = deg_to_rad(_throwAngle)
+	var rotation_axis = transform.basis.x
+	_throw_dir = forward_dir.rotated(rotation_axis, angle_rad).normalized()
 
-    # Predict throw path based on direction and draw it
-    var path = predict_ball_path(_ballVisual.global_position, _throw_dir*_throwForce)
-    _line3D.curve.clear_points()
-    for p in path:
-        _line3D.curve.add_point(to_local(p))
+	# Predict throw path based on direction and draw it
+	var path = predict_ball_path(_ballVisual.global_position, _throw_dir*_throwForce)
+	_line3D.curve.clear_points()
+	for p in path:
+		_line3D.curve.add_point(to_local(p))
 
 func DoShot() -> void:
-    SpawnBall()
-    _currentBall.apply_central_impulse(_throw_dir * _throwForce)
-    _ballVisual.visible = false
-    _line3D.visible = false
-    _throwAngle = _defaultShotAngle
-    _throwForce = _defaultShotForce
+	SpawnBall()
+	_currentBall.apply_central_impulse(_throw_dir * _throwForce)
+	_ballVisual.visible = false
+	_line3D.visible = false
+	_throwAngle = _defaultShotAngle
+	_throwForce = _defaultShotForce
 
-func predict_ball_path(start_position: Vector3, initial_velocity: Vector3, steps: int = 25, delta_t: float = 0.02) -> PackedVector3Array:
-    var GRAVITY: Vector3 = Vector3.DOWN * -get_gravity()
-    var path := PackedVector3Array()
-    var current_position := start_position
-    var current_velocity := initial_velocity
-    var time_step := delta_t
+func predict_ball_path(start_position: Vector3, initial_velocity: Vector3, steps: int = 50, delta_t: float = 0.02) -> PackedVector3Array:
+	var GRAVITY: Vector3 = Vector3.DOWN * -get_gravity()
+	var path := PackedVector3Array()
+	var current_position := start_position
+	var current_velocity := initial_velocity
+	var time_step := delta_t
 
-    for i in range(steps):
-        current_position += current_velocity * time_step
-        current_velocity += GRAVITY * time_step
-        path.push_back(current_position)
-    return path
+	for i in range(steps):
+		current_position += current_velocity * time_step
+		current_velocity += GRAVITY * time_step
+		path.push_back(current_position)
+	return path
